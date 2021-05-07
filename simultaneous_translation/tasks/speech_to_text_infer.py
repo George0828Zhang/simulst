default to use causal encoder. fixed simul agent for wait-k model.

simultaneous_translation/tasks/speech_to_text_infer.py
@@ -84,7 +84,7 @@ class SpeechToTextWInferenceTask(SpeechToTextTask):
         def decode(toks, escape_unk=False):
             s = self.tgt_dict.string(
                 toks.int().cpu(),
-                self.inference_cfg.post_process, # this will handle bpe for us.
+                self.inference_cfg.post_process,  # this will handle bpe for us.
                 # The default unknown string in fairseq is `<unk>`, but
                 # this is tokenized by sacrebleu as `< unk >`, inflating
                 # BLEU scores. Instead, we use a somewhat more verbose
@@ -99,7 +99,7 @@ class SpeechToTextWInferenceTask(SpeechToTextTask):
         gen_out = self.inference_step(generator, [model], sample, prefix_tokens=None)
         hyps, refs = [], []
         for i in range(len(gen_out)):
-            hyps.append(                
+            hyps.append(
                 decode(gen_out[i][0]["tokens"])
             )
             refs.append(
@@ -117,10 +117,10 @@ class SpeechToTextWInferenceTask(SpeechToTextTask):
             bleu_scorer = SacrebleuScorer(self.inference_cfg.eval_bleu_args)
             for h, r in zip(hyps, refs):
                 bleu_scorer.add_string(ref=r, pred=h)
-            
+
             ret["bleu"] = bleu_scorer.sacrebleu.corpus_bleu(
-                bleu_scorer.pred, [bleu_scorer.ref], 
-                tokenize="none" # use none because it's handled by SacrebleuScorer
+                bleu_scorer.pred, [bleu_scorer.ref],
+                tokenize="none"  # use none because it's handled by SacrebleuScorer
             )
 
         if self.inference_cfg.eval_wer:
@@ -129,8 +129,8 @@ class SpeechToTextWInferenceTask(SpeechToTextTask):
                 wer_scorer.add_string(ref=r, pred=h)
 
             ret["wer"] = {
-                "wv_errors": wer_scorer.distance, 
-                "w_errors": wer_scorer.distance, 
+                "wv_errors": wer_scorer.distance,
+                "w_errors": wer_scorer.distance,
                 "w_total": wer_scorer.ref_length
             }
 
@@ -145,7 +145,7 @@ class SpeechToTextWInferenceTask(SpeechToTextTask):
             if torch.is_tensor(result):
                 result = result.cpu()
             return result
-        
+
         if self.inference_cfg.eval_bleu:
 
             counts, totals = [], []
@@ -188,7 +188,7 @@ class SpeechToTextWInferenceTask(SpeechToTextTask):
 
             metrics.log_scalar("_w_errors", w_errors)
             metrics.log_scalar("_wv_errors", wv_errors)
-            metrics.log_scalar("_w_total", w_total)            
+            metrics.log_scalar("_w_total", w_total)
 
             if w_total > 0:
                 metrics.log_derived(

simultaneous_translation/tasks/waitk_sequence_generator.py
@@ -300,13 +300,13 @@ class WaitkSequenceGenerator(nn.Module):
                     encoder_outs, reorder_state
                 )
 
-            context_size = step + self.waitk*self.pre_decision_ratio
+            context_size = step + self.waitk * self.pre_decision_ratio
             contexts.append(context_size)
 
             lprobs, avg_attn_scores = self.model.forward_decoder(
                 tokens[:, : step + 1],
                 self.model.slice_source(encoder_outs, context_size),
-                incremental_states, # ?
+                incremental_states,  # ?
                 self.temperature,
             )
 
@@ -326,7 +326,7 @@ class WaitkSequenceGenerator(nn.Module):
             # handle max length constraint
             if step >= max_len:
                 lprobs[:, : self.eos] = -math.inf
-                lprobs[:, self.eos + 1 :] = -math.inf
+                lprobs[:, self.eos + 1:] = -math.inf
 
             # handle prefix tokens (possibly with different lengths)
             if (
@@ -553,7 +553,7 @@ class WaitkSequenceGenerator(nn.Module):
         if eos_mask.any():
             # validate that the first beam matches the prefix
             first_beam = tokens[eos_mask].view(-1, beam_size, tokens.size(-1))[
-                :, 0, 1 : step + 1
+                :, 0, 1: step + 1
             ]
             eos_mask_batch_dim = eos_mask.view(-1, beam_size)[:, 0]
             target_prefix = prefix_tokens[eos_mask_batch_dim][:, :step]
@@ -599,12 +599,12 @@ class WaitkSequenceGenerator(nn.Module):
         # tokens is (batch * beam, max_len). So the index_select
         # gets the newly EOS rows, then selects cols 1..{step + 2}
         tokens_clone = tokens.index_select(0, bbsz_idx)[
-            :, 1 : step + 2
+            :, 1: step + 2
         ]  # skip the first index, which is EOS
 
         tokens_clone[:, step] = self.eos
         attn_clone = (
-            attn.index_select(0, bbsz_idx)[:, :, 1 : step + 2]
+            attn.index_select(0, bbsz_idx)[:, :, 1: step + 2]
             if attn is not None
             else None
         )