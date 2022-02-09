84012444c67b591959ed0658b605205e5174459c
Chih-Chiang Chang <cc.chang0828@gmail.com>
2022-02-10 02:37:07 +0800

    changed ssnt loss as submodule.

--- codebase/criterion/ssnt_criterion.py
+++ codebase/criterion/ssnt_criterion.py
@@ -5,10 +5,7 @@
 
 from dataclasses import dataclass, field
 import torch
-import torch.nn.functional as F
 from typing import Optional
-from fairseq import metrics
-# from fairseq.logging.meters import safe_round
 from fairseq.criterions import (
     register_criterion,
 )
@@ -17,7 +14,7 @@ from fairseq.criterions.label_smoothed_cross_entropy import (
     LabelSmoothedCrossEntropyCriterion
 )
 import logging
-from codebase.criterion.monotonic import ssnt_loss, ssnt_loss_mem
+from codebase.criterion.ssnt_loss import ssnt_loss, ssnt_loss_mem
 
 logger = logging.getLogger(__name__)
 
@@ -31,6 +28,13 @@ class SSNTCriterionConfig(LabelSmoothedCrossEntropyCriterionConfig):
     memory_efficient: Optional[bool] = field(
         default=False,
         metadata={"help": "concatenate the 'targets' dimension to improve efficiency"},
+    )
+    fastemit_lambda: Optional[float] = field(
+        default=0.01,
+        metadata={
+            "help":
+            "scales the non-blank prediction gradient by 1 + lambda to encourage faster emission"
+        },
     )
 
 
@@ -52,6 +56,7 @@ class SSNTCriterion(LabelSmoothedCrossEntropyCriterion):
         self.eos_idx = task.target_dictionary.eos()
         # self.zero_infinity = cfg.zero_infinity
         self.memory_efficient = cfg.memory_efficient
+        self.fastemit_lambda = cfg.fastemit_lambda
 
     def forward(self, model, sample, reduce=True):
         net_output = model(**sample["net_input"])
@@ -61,10 +66,6 @@ class SSNTCriterion(LabelSmoothedCrossEntropyCriterion):
         sample_size = (
             sample["target"].size(0) if self.sentence_avg else sample["ntokens"]
         )
-        # sample_size = 1
-        # if not self.sentence_avg:
-        #     factor = sample["ntokens"] / sample["target"].size(0)
-        #     loss /= factor
 
         logging_output = {
             "loss": loss.data,
@@ -86,9 +87,7 @@ class SSNTCriterion(LabelSmoothedCrossEntropyCriterion):
         )
         targets = sample["target"]
 
-        log_emit = F.logsigmoid(
-            net_output[1]["log_emit"]
-        )
+        emit_logits = net_output[1]["emit_logits"]
 
         bsz = targets.size(0)
         if self.memory_efficient:
@@ -99,21 +98,21 @@ class SSNTCriterion(LabelSmoothedCrossEntropyCriterion):
             if lprobs.size(0) != _bsz:
                 raise RuntimeError(
                     f'batch size error: lprobs shape={lprobs.size()}, bsz={_bsz}')
-            if log_emit.size(0) != _bsz:
+            if emit_logits.size(0) != _bsz:
                 raise RuntimeError(
-                    f'batch size error: log_emit shape={log_emit.size()}, bsz={_bsz}')
+                    f'batch size error: emit_logits shape={emit_logits.size()}, bsz={_bsz}')
         else:
             target_lengths = sample["target_lengths"]
             if lprobs.size(0) != bsz:
                 raise RuntimeError(
                     f'batch size error: lprobs shape={lprobs.size()}, bsz={bsz}')
-            if log_emit.size(0) != bsz:
+            if emit_logits.size(0) != bsz:
                 raise RuntimeError(
-                    f'batch size error: log_emit shape={log_emit.size()}, bsz={bsz}')
+                    f'batch size error: emit_logits shape={emit_logits.size()}, bsz={bsz}')
 
         max_src = lprobs.size(-2)
         lprobs = lprobs.contiguous()
-        log_emit = log_emit.contiguous()
+        emit_logits = emit_logits.contiguous()
 
         # get subsampling padding mask & lengths
         if net_output[1]["padding_mask"] is not None:
@@ -127,64 +126,21 @@ class SSNTCriterion(LabelSmoothedCrossEntropyCriterion):
             loss = ssnt_loss_mem(
                 lprobs,
                 targets,
-                log_emit,
                 input_lengths,
                 target_lengths,
-                reduction="sum"
+                emit_logits=emit_logits,
+                reduction="sum",
+                fastemit_lambda=self.fastemit_lambda
             )
         else:
             loss = ssnt_loss(
                 lprobs,
                 targets,
-                log_emit,
                 input_lengths,
                 target_lengths,
-                reduction="sum"
+                emit_logits=emit_logits,
+                reduction="sum",
+                fastemit_lambda=self.fastemit_lambda
             )
 
         return loss, loss.detach()
-
-    @classmethod
-    def reduce_metrics(cls, logging_outputs) -> None:
-        """Aggregate logging outputs from data parallel training."""
-        super().reduce_metrics(logging_outputs)
-
-        def sum_logs(key):
-            import torch
-            result = sum(log.get(key, 0) for log in logging_outputs)
-            if torch.is_tensor(result):
-                result = result.cpu()
-            return result
-
-        ntokens = sum_logs("ntokens")
-        npads = sum_logs("npads")
-        ntotal = ntokens + npads
-        metrics.log_scalar(
-            "pad_rate", npads / ntotal, ntotal, round=3
-        )
-
-        # nsentences = sum_logs("nsentences")
-        # recall = sum_logs("recall")
-        # precision = sum_logs("precision")
-        # blank_rate = sum_logs("blank_rate")
-
-        # metrics.log_scalar(
-        #     "blank_rate", blank_rate / nsentences, nsentences, round=3
-        # )
-
-        # metrics.log_scalar("_recall", recall / nsentences, nsentences)
-        # metrics.log_scalar("_precision", precision / nsentences, nsentences)
-
-        # def calc_f1(meters):
-        #     """ this is approx. """
-        #     r = meters["_recall"].avg
-        #     p = meters["_precision"].avg
-        #     if r + p > 0:
-        #         return safe_round(2 * p * r / (p + r), 3)
-        #     else:
-        #         return 0
-
-        # metrics.log_derived(
-        #     "f1",
-        #     calc_f1,
-        # )
