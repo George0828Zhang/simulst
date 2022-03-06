6ff2515b9420abebb6adc24443b7d4b1320008c5
Chih-Chiang Chang <cc.chang0828@gmail.com>
2022-03-06 14:00:07 +0800

    update eval asr script

--- eval/eval_asr.sh
+++ eval/eval_asr.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 TASK=ctc_s2s_asr
-SPLIT=tst-COMMON
+SPLIT=dev
 EXP=$(realpath ../exp)
 . ${EXP}/data_path.sh
 CONF=$DATA/config_st.yaml
-INF=${EXP}/infer_asr.yaml
 CHECKDIR=${EXP}/checkpoints/${TASK}
-RESULTS=asr.${SPLIT}.results/
+RESULTS=asr.${SPLIT}.results
 AVG=true
 
-EXTRAARGS=""
+EXTRAARGS="--scoring wer --wer-remove-punct --wer-lowercase --wer-tokenizer 13a --beam 5 --max-len-a 0.1 --max-len-b 10"
 
 if [[ $AVG == "true" ]]; then
     CHECKPOINT_FILENAME=avg_best_5_checkpoint.pt
@@ -22,19 +21,12 @@ fi
 
 mkdir -p ${RESULTS}
 
-tsv=${DATA}/${SPLIT}_st.tsv
-tail +2 ${tsv} | cut -f2 > ${RESULTS}/feats.${TGT}
-tail +2 ${tsv} | cut -f4 > ${RESULTS}/refs.${TGT}
-cat ${RESULTS}/feats.${TGT} | \
-    python interactive.py ${DATA} --user-dir ${USERDIR} \
+python generate.py ${DATA} --user-dir ${USERDIR} \
         --config-yaml ${CONF} \
         --gen-subset ${SPLIT}_st \
         --task speech_to_text_infer --do-asr \
-        --buffer-size 1500 --batch-size 32 \
-        --inference-config-yaml ${INF} \
+        --results-path ${RESULTS} \
         --path ${CHECKDIR}/${CHECKPOINT_FILENAME} \
-        ${EXTRAARGS} | \
-    grep -E "D-[0-9]+" | \
-    cut -f3 > ${RESULTS}/hyps.${TGT}
-script=$(which wer)
-python ${script} ${RESULTS}/refs.${TGT} ${RESULTS}/hyps.${TGT} | tee ${RESULTS}/interactive_score.txt
+        --model-overrides '{"load_pretrained_encoder_from": None}' \
+        ${EXTRAARGS}
+tail -1 ${RESULTS}/generate-${SPLIT}_st.txt
